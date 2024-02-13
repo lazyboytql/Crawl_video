337fee2f7a6e29329851cdd55ff2025950fc4421
lazyboytql <89647416+lazyboytql@users.noreply.github.com>
2024-02-14 00:35:53 +0700

    Code Cào Cào Data video YTB

--- Crawl_videoYTB.py
+++ Crawl_videoYTB.py
@@ -1,12 +1,9 @@
 from googleapiclient.discovery import build
 import datetime
-
 import psycopg2
-
 
 api_key = 'AIzaSyBwbMbfzilKXP3jlj10S_LjhKYkMVGXTFI'
 video_id = '7HGo9Jb3OXE'
-
 
 conn = psycopg2.connect(
     dbname="postgres",
@@ -16,18 +13,13 @@ conn = psycopg2.connect(
     port="5433"
 )
 
-
 cur = conn.cursor()
-
-
 youtube = build('youtube', 'v3', developerKey=api_key)
-
 
 video_response = youtube.videos().list(
     part='snippet,statistics',
     id=video_id
 ).execute()
-
 
 video_snippet = video_response['items'][0]['snippet']
 video_statistics = video_response['items'][0]['statistics']
@@ -38,7 +30,6 @@ view_count = video_statistics['viewCount']
 like_count = video_statistics.get('likeCount', 0)
 comment_count = video_statistics.get('commentCount', 0)
 
-
 published_time = datetime.datetime.strptime(published_time, "%Y-%m-%dT%H:%M:%SZ")
 
 cur.execute("""
@@ -47,7 +38,6 @@ cur.execute("""
 """, (title, published_time, view_count, like_count, comment_count))
 
 conn.commit()
-
 cur.close()
 conn.close()
 
