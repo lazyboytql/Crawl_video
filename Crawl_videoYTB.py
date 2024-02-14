234df936c5aa8022bd9690569584206452861131
lazyboytql <89647416+lazyboytql@users.noreply.github.com>
2024-02-14 13:28:10 +0700

    Update code

--- Crawl_videoYTB.py
+++ Crawl_videoYTB.py
@@ -2,44 +2,63 @@ from googleapiclient.discovery import build
 import datetime
 import psycopg2
 
-api_key = 'AIzaSyBwbMbfzilKXP3jlj10S_LjhKYkMVGXTFI'
-video_id = '7HGo9Jb3OXE'
+API_KEY = 'AIzaSyBwbMbfzilKXP3jlj10S_LjhKYkMVGXTFI'
+VIDEO_IDS = ['7HGo9Jb3OXE', 'HJDwo7J8oL4','au7J8FacCK8','aGbfU4GiI9s','nLRL_NcnK-4','YZ5tOe7y9x4','0MhVkKHYUAY','YgCq0Y6Mhh0'] 
+DB_CONFIG = {
+    "dbname": "postgres",
+    "user": "postgres",
+    "password": "Nokia_2730",
+    "host": "localhost",
+    "port": "5433"
+}
 
-conn = psycopg2.connect(
-    dbname="postgres",
-    user="postgres",
-    password="Nokia_2730",
-    host="localhost",
-    port="5433"
-)
+def fetch_videos(video_ids, api_key, db_config, table_name):
+    conn = psycopg2.connect(**db_config)
+    youtube = build('youtube', 'v3', developerKey=api_key)
+    
+    with conn, conn.cursor() as cur:
+        for video_id in video_ids:
+            video_data = get_video_data(youtube, video_id)
+            if video_data:
+                insert_video_data(cur, table_name, video_data)
 
-cur = conn.cursor()
-youtube = build('youtube', 'v3', developerKey=api_key)
+        print("Data inserted into PostgreSQL table successfully !!!")
 
-video_response = youtube.videos().list(
-    part='snippet,statistics',
-    id=video_id
-).execute()
+def get_video_data(youtube, video_id):
+    video_response = youtube.videos().list(
+        part='snippet,statistics',
+        id=video_id
+    ).execute()
 
-video_snippet = video_response['items'][0]['snippet']
-video_statistics = video_response['items'][0]['statistics']
+    if 'items' in video_response and video_response['items']:
+        video_snippet = video_response['items'][0]['snippet']
+        video_statistics = video_response['items'][0]['statistics']
 
-title = video_snippet['title']
-published_time = video_snippet['publishedAt']
-view_count = video_statistics['viewCount']
-like_count = video_statistics.get('likeCount', 0)
-comment_count = video_statistics.get('commentCount', 0)
+        title = video_snippet['title']
+        published_time = video_snippet['publishedAt']
+        view_count = video_statistics.get('viewCount', 0)
+        like_count = video_statistics.get('likeCount', 0)
+        comment_count = video_statistics.get('commentCount', 0)
 
-published_time = datetime.datetime.strptime(published_time, "%Y-%m-%dT%H:%M:%SZ")
+        published_time = datetime.datetime.strptime(published_time, "%Y-%m-%dT%H:%M:%SZ")
 
-cur.execute("""
-    INSERT INTO ytb (title, published_time, view_count, like_count, comment_count)
-    VALUES (%s, %s, %s, %s, %s)
-""", (title, published_time, view_count, like_count, comment_count))
+        return {
+            'title': title,
+            'published_time': published_time,
+            'view_count': view_count,
+            'like_count': like_count,
+            'comment_count': comment_count
+        }
 
-conn.commit()
-cur.close()
-conn.close()
+    else:
+        print(f"No data found for video ID: {video_id}")
+        return None
 
-print("Data inserted into PostgreSQL table successfully !!!")
- 
+def insert_video_data(cur, table_name, video_data):
+    cur.execute(f"""
+        INSERT INTO {table_name} (title, published_time, view_count, like_count, comment_count)
+        VALUES (%s, %s, %s, %s, %s)
+    """, (video_data['title'], video_data['published_time'], video_data['view_count'], video_data['like_count'], video_data['comment_count']))
+
+TABLE_NAME = "ytb"
+fetch_videos(VIDEO_IDS, API_KEY, DB_CONFIG, TABLE_NAME)
